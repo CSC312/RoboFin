dd664c703e6e2f9abe06ecd5f28b38d45cc4d56f
John Claude <unjohnclaude@gmail.com>
2018-04-21 00:46:03 +0200

    some functionality

--- RoboFin/logInPage.xaml.cs
+++ RoboFin/logInPage.xaml.cs
@@ -12,10 +12,23 @@ namespace RoboFin
             InitializeComponent();
 
             logUserButton.Clicked += async (sender, e) => {
-
+                string usrname = usrnameField.Text;
+                string pwd = pwdField.Text;
+                if(checkCredentials(usrname,pwd) == false){
+                    errorLabel.Text = "Incorrect Username/Password Combination!";
+                    errorLabel.IsVisible = true;
+                }
+                errorLabel.IsVisible = false;
                 await Navigation.PushAsync(new HomePage());
             };
 
+        }
+
+        public bool checkCredentials(string username, string password){
+            if(username != "a" && password != "a" ){
+                return false;
+            }
+            return true;
         }
     }
 }
