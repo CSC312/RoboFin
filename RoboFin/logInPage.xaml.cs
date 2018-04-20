fa266d268e4c0acd15dc3211ae1965bae6b4a29a
John Claude <unjohnclaude@gmail.com>
2018-04-20 21:33:03 +0200

    Home Screen

---
+++ RoboFin/logInPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace RoboFin
+{
+    public partial class logInPage : ContentPage
+    {
+        public logInPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
